2a27826cea410bb67e4f8e393e19e2e87a23d5d0
Mike Gregory <meikura@gmail.com>
2015-11-07 21:20:59 +0000

    Wrote singleton trait

--- classes/singleton.php
+++ classes/singleton.php
@@ -1,5 +1,21 @@
 <?php
 
-class singleton {
-	// WRITE SINGLETON CLASS
+trait singleton {
+    /** @var array Singleton instances. */
+    protected static $instances = array();
+
+    /**
+     * Return singleton instance of called class
+     *
+     * @return class instance
+     */
+    public static function instance() {
+        $class = get_called_class();
+
+        if (! isset(self::$instances[$class])) {
+            self::$instances[$class] = new static;
+        }
+
+        return self::$instances[$class];
+    }
 }
