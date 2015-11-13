4de4fb4d6680369ddeaa30a0536f320cde320748
Mike Gregory <meikura@gmail.com>
2015-11-13 07:56:14 +0000

    changes to structure

--- applications/authentication/controllers/auth.controller.php
+++ applications/authentication/controllers/auth.controller.php
@@ -1,5 +1,3 @@
 <?php
 
-class auth {
-	// code
-}
+// code
--- helpers/appLoader.php
+++ helpers/appLoader.php
@@ -1,18 +1,33 @@
 <?php
 
-/* Return an instance of an app's controller class
+/* Return an instance of an app's class
+ *
+ * @param string @appname Name of application
+ * @param string @controller Name of class
+ * @return object Class
+ */
+
+function loadClass ($appname, $class) {
+	$path = dirname(dirname(__FILE__)) . '/applications/' . $appname . '/classes/' . $class . '.class.php';
+	if (file_exists($path)) {
+		include_once $path;
+		$return = new $class();
+		return $return;
+	} else {
+		throw new Exception ("Class $class in $appname does not exist");
+	}
+}
+
+/* Load an app's controller functions
  *
  * @param string @appname Name of application
  * @param string @controller Name of controller
- * @return object Controller
+ * @return void
  */
-
-function loadController ($appname, $controller) {
-	$path = dirname(dirname(__FILE__)) . '/applications/' . $appname . '/controllers/' . $controller . '.controller.php';
+function loadHelper ($appname, $controller) {
+	$path = dirname(dirname(__FILE__)) . '/applications/' . $appname . '/helpers/' . $controller . '.helper.php';
 	if (file_exists($path)) {
-		include_once $path;
-		$return = new $controller();
-		return $return;
+		require_once $path;
 	} else {
 		throw new Exception ("Controller $controller in $appname does not exist");
 	}
