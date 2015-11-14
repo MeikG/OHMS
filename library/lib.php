f7b1ac95efec5217c8b67f60606046465d384cc4
Mike Gregory <meikura@gmail.com>
2015-11-14 16:28:57 +0000

    added verification that config.php has been set-up

--- library/lib.php
+++ library/lib.php
@@ -8,3 +8,10 @@ require_once dirname(dirname(__FILE__)) . '/library/config.php';
 require_once dirname(dirname(__FILE__)) . '/helpers/appLoader.php';
 require_once dirname(dirname(__FILE__)) . '/library/apiRouter.php';
 require_once dirname(dirname(__FILE__)) . '/helpers/authorisation.php';
+
+// If the configuration file has not been set up, display an error.
+global $config;
+if (! $config) {
+	throw new \Exception('Configuration file not found. Please follow the instructions in config_default.php.');
+	die();
+}
