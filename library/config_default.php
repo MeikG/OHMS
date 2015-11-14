f5ff3e8988c41bd43ee9af45725c142a067601f8
Mike Gregory <meikura@gmail.com>
2015-11-14 16:29:47 +0000

    added instructions

--- library/config_default.php
+++ library/config_default.php
@@ -4,6 +4,12 @@ namespace mgregory;
 
 /* CONFIGURATION VARIABLES */
 
+// Rename this file to config.php
+// ALWAYS KEEP IT SECRET.
+
+// Set default timezone for this server.
+date_default_timezone_set('Europe/London');
+
 $config = new stdClass;
 
 // Define $config site variables
