f25a918c6e3948498554a83ea9771512d2bb1b1d
Mike Gregory <meikura@gmail.com>
2016-06-23 19:51:04 +0100

    removed whitespace in UsersController

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -16,12 +16,10 @@ module.exports = {
     var email = req.param('email');
     var fullName = req.param('fullName');
     var password = req.param('password');
-
     // Ensure that we've got some data for each of these fields.
     if (! email || ! fullName || ! password) {
       return res.badRequest();
     }
-
     // Create the user and store the record.
     Users.createUser(email, fullName, password, function(err, user) {
       if (err) {
@@ -37,7 +35,6 @@ module.exports = {
       delete user.password;
       delete user.createdAt;
       delete user.updatedAt;
-
       // Create a JWT and sign the user into the system.
       Tokens.createJwt(user, function (err, jwt) {
         if (err) return res.serverError(err);
@@ -59,12 +56,10 @@ module.exports = {
     // Grab the form data.
     var email = req.param('email');
     var password = req.param('password');
-
     // Ensure that we've got some data for each of these fields.
     if (! email || ! password) {
       return res.badRequest();
     }
-
     // Validate that the credentials are correct.
     Users.validateUser(email, password, function(err, user) {
       if (err) {
@@ -78,7 +73,6 @@ module.exports = {
       // Clean up the user object further.
       delete user.createdAt;
       delete user.updatedAt;
-
       // Convert the user object into a JSON Web Token.
       Tokens.createJwt(user, function (err, jwt) {
         if (err) return res.serverError(err);
@@ -101,12 +95,10 @@ module.exports = {
     var email = req.param('email');
     var oldPassword = req.param('oldPassword');
     var newPassword = req.param('newPassword');
-
     // Ensure that we've got some data for each of these fields.
     if (! email || ! oldPassword || ! newPassword) {
       return res.badRequest();
     }
-
     // Update the password with the new one.
     Users.updatePassword(email, oldPassword, newPassword, function(err, correct) {
       if (err) {
