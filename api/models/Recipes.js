a87d507b3e3748fff4c4139e8cef34919a1c1a12
Mike Gregory <meikura@gmail.com>
2016-06-23 19:51:49 +0100

    Added methods to find recipes

--- api/models/Recipes.js
+++ api/models/Recipes.js
@@ -16,8 +16,7 @@ module.exports = {
     recipeName: 'string',
     recipeDescription: 'string',
     writtenBy: {
-      model: 'Users',
-      unique: true
+      model: 'Users'
     },
     ingredients: {
       collection: 'RecipeIngredients',
@@ -41,11 +40,59 @@ module.exports = {
     // Retrieve the user from the JWT.
     Tokens.getClaims(token, function (err, claims) {
       if (err) return cb(err);
-      Recipes.create({recipeName, recipeDescription, writtenBy: claims.id}).exec(function(err, recipe) {
+      Recipes.findOne({recipeName, writtenBy: claims.id}).exec(function (err, duplicate) {
         if (err) return cb(err);
-        return cb(null, recipe);
+        if (duplicate) return cb(400);
+        Recipes.create({recipeName, recipeDescription, writtenBy: claims.id}).exec(function (err, recipe) {
+          if (err) return cb(err);
+          return cb(null, recipe);
+        });
       });
     });
   },
 
+  /*
+   *  Returns either all recipes if user is null, or a specific recipe.
+   *
+   *  @param {int} User id or null.
+   *  @returns {array} Array of all recipes in the system.
+   */
+  findRecipes(id, cb) {
+    // If null is provided, search for all recipes.
+    var search = (id === null? {} : {id});
+
+    Recipes.find(search)
+    .populate('writtenBy')
+    .exec(function (err, recipes) {
+      if (err) return cb(err);
+      recipes.forEach(function (recipe) {
+        delete recipe.writtenBy.password;
+        delete recipe.writtenBy.createdAt;
+        delete recipe.writtenBy.updatedAt;
+      });
+      if (recipes.length === 0) return cb(404);
+      if (id !== null) return cb(null, recipes[0]);
+      return cb(null, recipes);
+    });
+  },
+
+  /*
+   *  Find all recipes made by a specific user.
+   *
+   *  @param {int} user id
+   *  @returns {array} Array of recipes.
+   */
+  findMine(id, cb) {
+    Recipes.find({writtenBy: id})
+    .populate('writtenBy')
+    .exec(function (err, recipes) {
+      if (err) return cb(err);
+      recipes.forEach(function (recipe) {
+        delete recipe.writtenBy.password;
+        delete recipe.writtenBy.createdAt;
+        delete recipe.writtenBy.updatedAt;
+      });
+      return cb(null, recipes);
+    });
+  }
 };
