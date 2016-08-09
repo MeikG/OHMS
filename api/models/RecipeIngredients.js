2636af0400040bc78b5bfab2b4bda1585c5b2375
Mike Gregory <meikura@gmail.com>
2016-08-09 16:38:29 +0100

    Refactored ingredients model

--- api/models/RecipeIngredients.js
+++ api/models/RecipeIngredients.js
@@ -24,27 +24,28 @@ module.exports = {
   /*
    *  Creates an ingredient for a recipe.
    *
-   *  @param {int} recipe id
-   *  @param {int} amount
-   *  @param {string} measurement
-   *  @param {string} name of ingredient
+   *  @param {object} ingredient
    *  @param {string} jwt token
    *  @returns {object} Recipe Ingredient
    */
-  createIngredient: function (id, amount, measurement, name, token, cb) {
+  createIngredient: function (ingredient, token, cb) {
+    var ingredientObj = {
+      recipe: ingredient.recipe || null,
+      name: ingredient.name || null,
+      amount: ingredient.amount || null,
+      measurement: ingredient.measurement || null
+    };
     Tokens.getClaims(token, function (err, claims) {
-      Recipes.findOne({id})
+      Recipes.findOne({id: ingredient.recipe})
       .exec(function (err, record) {
         if (err) return cb(err);
         // If the user did not write the recipe, fail to add the ingredient.
-        if (record.writtenBy !== claims.id) return cb(null, null);
-        Recipes.findOne({id}).exec(function (err, exists) {
+        if (! record || record.writtenBy !== claims.id) return cb('denied');
+        RecipeIngredients
+        .create(ingredientObj)
+        .exec(function (err, newIngredient) {
           if (err) return cb(err);
-          if (! exists) return cb(null, null);
-          RecipeIngredients.create({recipe: id, amount, measurement, name}).exec(function (err, ingredient) {
-            if (err) return cb(err);
-            return cb(null, ingredient);
-          });
+          return cb(null, newIngredient);
         });
       });
     });
@@ -58,7 +59,13 @@ module.exports = {
    *  @param {string} jwt token
    *  @returns {object} updated ingredient.
    */
-  editIngredient: function (id, ingredient, token, cb) {
+  editIngredient: function (id, preUpdate, token, cb) {
+    var update = preUpdate;
+    // Do not allow the user to edit these fields.
+    delete update.id;
+    delete update.recipe;
+    delete update.createdAt;
+    delete update.updatedAt;
     // Grab the token claims.
     Tokens.getClaims(token, function (err, claims) {
       if (err) return cb(err);
@@ -67,11 +74,37 @@ module.exports = {
       .exec(function (err, record) {
         if (err) return cb(err);
         // Compare against the recipe writer.
-        if (record.recipe.writtenBy !== claims.id) return cb(null, null);
-        RecipeIngredients.update({id}, ingredient)
+        if (! record || record.recipe.writtenBy !== claims.id) return cb('forbidden');
+        RecipeIngredients.update({id}, update)
         .exec(function (err, updatedIngredient) {
           if (err) return cb(err);
           return cb(null, updatedIngredient);
+        });
+      });
+    });
+  },
+
+  /*
+   *  Deletes an ingredient from a recipe.
+   *
+   *  @param {int} id
+   *  @param {string} jwt token
+   *  @returns {boolean} if completed successfully.
+   */
+  deleteIngredient: function (id, token, cb) {
+    // Grab the token claims.
+    Tokens.getClaims(token, function (err, claims) {
+      if (err) return cb(err);
+      RecipeIngredients.findOne({id})
+      .populate('recipe')
+      .exec(function (err, record) {
+        if (err) return cb(err);
+        // Ensure that the user attempting to delete the ingredient owns the recipe.
+        if (! record || record.recipe.writtenBy !== claims.id) return cb('forbidden');
+        RecipeIngredients.destroy({id})
+        .exec(function (err) {
+          if (err) return cb(err);
+          return cb(null, true);
         });
       });
     });
